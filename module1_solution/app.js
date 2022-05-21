93373a8c87c8baca5cd3f7f4c281004052f142c0
jagadish <jagapgowda10@gmail.com>
2022-05-21 10:53:24 +0530

    Final code

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -6,18 +6,22 @@
 
     function checkItems($scope) {
         $scope.items="";
-        $scope.message=""
+        $scope.message="";
+        $scope.color=""
 
         $scope.checkCount = function() {
             $scope.itemArray = $scope.items.split(',').length;
             if( $scope.items.length==0){
                 $scope.message = "Please enter data first"
+                $scope.color="red"
             }
             else if($scope.itemArray>=1 && $scope.itemArray<=3) {
                 $scope.message = "Enjoy!"
+                $scope.color="green"
             }
             else {
                 $scope.message = "Too much!"
+                $scope.color="green"
             }
         }
     };
